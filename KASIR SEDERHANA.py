958d84f427a047b907b313b556259c55162518b5
angguncom <148510778+angguncom@users.noreply.github.com>
2023-10-20 10:20:15 +0800

    Add files via upload

---
+++ KASIR SEDERHANA.py
@@ -0,0 +1,27 @@
+PIN=input ("masukkan pin anda")
+
+if PIN=="123456":
+    saldo =549000
+    print("SELAMAT DATANG DI BANK BRI")
+    print("1.TARIK TUNAI")
+    print("2.STOR TUNAI")
+    print("3.CEKK SALDO")
+    print("4.KELUAR")
+    MENU = input("pilih menu yang akan dipilih : ")
+    if MENU =='1':
+       print("anda memilih menu tarik tunai")
+       JML_PENARIKAN = int(input("masukkan jumlah penarikan : "))
+       SISA = saldo - JML_PENARIKAN
+       print("jumlah penarikan : ", JML_PENARIKAN)
+       print("sisa saldo : ", SISA)
+    elif MENU == '2' :
+       print("anda memilih menu stor tunai")
+    elif MENU == '3' :
+       print("anda memilih menu cek saldo")
+       print("saldo anda adalah ", saldo)
+    elif MENU =='4' :
+       print ("keluar")
+
+else:
+    print("PIN ANDA SALAH")
+        
