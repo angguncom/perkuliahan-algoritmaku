2f121a369dee4e1f3f8f9db7fe46c90c0d78384e
angguncom <148510778+angguncom@users.noreply.github.com>
2023-10-20 10:54:48 +0800

    Add files via upload

---
+++ proyek 1.py
@@ -0,0 +1,15 @@
+>>> hasilpenjumlahan= 5+2
+>>> print(hasilpenjumlahan)
+7
+>>> hasilpengurangan= 5-2
+>>> print(hasilpengurangan)
+3
+>>> hasilperkalian= 5*2
+>>> print(hasilperkalian)
+10
+>>> hasilpembagian= 5/2
+>>> print(hasilpembagian)
+2.5
+>>> hasilmodulus= 5%2
+>>> print(hasilmodulus)
+1
