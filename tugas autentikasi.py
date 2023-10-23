1efda42937e6a48a42d69749f12a1215af8a7669
angguncom <148510778+angguncom@users.noreply.github.com>
2023-10-23 09:31:27 +0800

    Add files via upload

---
+++ tugas autentikasi.py
@@ -0,0 +1,10 @@
+username = input('masukkan username: ')
+password =int(input("masukkan password: "))
+code_username = 'Anggun sindiana'
+code_password = 12345
+
+
+if username == code_username and password == code_password:
+    print("selamat datang",username)
+else:
+    print("username atau password salah")
